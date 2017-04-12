edb32446da77d8bca260e7552ba9976032af3e3a
Andrea Parodi <andrea@parro.it>
2017-04-12 20:36:08 +0200

    chore

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+export PATH=${0%/*}/node_modules/.bin:$PATH
 export package_author=`yarn config get init-author`
 export template=$1
 export package_name=$2
@@ -5,8 +6,8 @@ export package_name=$2
 echo scaffolding $package_name using $template as model
 
 # clone from author template repo
-git clone https://github.com/$package_author/$template.git $package_name
+git clone https://github.com/$package_author/$template.git $package_name &&
 
-cd $package_name
+cd $package_name &&
 
 sh init.sh
