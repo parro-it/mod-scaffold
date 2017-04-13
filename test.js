40c58a589e66db4faa6dd83fa957c4e36157e0e6
Andrea Parodi <andrea@parro.it>
2017-04-13 21:32:55 +0200

    fix test

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ const bin = `${__dirname}/init.sh`;
 test('init template script is executed', async t => {
 	const {stdout} = await execa(bin, ['test-template','test-result']);
 	const lines = stdout.split('\n');
-	t.is(lines[1], 'the answer is 42');
+	t.is(lines[lines.length - 1], 'the answer is 42');
 });
 
